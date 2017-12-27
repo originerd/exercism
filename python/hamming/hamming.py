fa938d44d14511da98c0c97e09e846aa39d4be00
Jitae Kim <originerd@gmail.com>
2017-12-27 20:51:04 +0900

    Apply review for hamming with python

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b): raise ValueError("Two strands must be same lengths")
 
-    return len([x for x in zip(list(strand_a), list(strand_b)) if x[0] != x[1]])
+    return len([x for x in zip(strand_a, strand_b) if x[0] != x[1]])
